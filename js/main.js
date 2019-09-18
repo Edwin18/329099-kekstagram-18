fb5d76c818bb1e5cc89e401aced150509cb910a1
Edwin18 <igreenbergi78@gmail.com>
2019-09-18 19:33:37 +0300

    HotFix

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var COMMENTS_ARRAY = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+// var COMMENTS_ARRAY = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 var COMMENTS_COUNT = 2;
 var MIN_LIKES = 15;
 var MAX_LIKES = 250;
@@ -14,24 +14,24 @@ var getRandomNumber = function (min, max) {
   return Math.round(rand);
 };
 
-var getRandomComment = function (commentsArray, commentsCount) {
-  var tempComments = '';
+// var getRandomComment = function (commentsArray, commentsCount) {
+//   var tempComments = '';
 
-  for (var i = 0; i <= getRandomNumber(0, commentsCount - 1); i++) {
-    tempComments = tempComments + commentsArray[getRandomNumber(0, commentsArray.length - 1)];
-  }
+//   for (var i = 0; i <= getRandomNumber(0, commentsCount - 1); i++) {
+//     tempComments = tempComments + commentsArray[getRandomNumber(0, commentsArray.length - 1)];
+//   }
 
-  return tempComments;
-};
+//   return tempComments;
+// };
 
-var getPhotoObject = function (minLikes, maxLikes, commentsArray, commentsCount, photoCount) {
+var getPhotoObject = function (minLikes, maxLikes, commentsCount, photoCount) {
   var photoSetup = [];
 
   for (var i = 0; i < photoCount; i++) {
     var tempObject = {};
     tempObject.url = 'photos/' + (i + 1) + '.jpg';
     tempObject.likes = getRandomNumber(minLikes, maxLikes);
-    tempObject.comments = getRandomComment(commentsArray, commentsCount);
+    tempObject.comments = getRandomNumber(1, commentsCount);
     photoSetup.push(tempObject);
     tempObject = {};
   }
@@ -49,14 +49,14 @@ var getPhotoTemplate = function (photo) {
   return photoElement;
 };
 
-var renderPhotos = function (minLikes, maxLikes, commentsArray, commentsCount, photoCount) {
+var renderPhotos = function (minLikes, maxLikes, commentsCount, photoCount) {
   var fragment = document.createDocumentFragment();
 
   for (var i = 0; i < photoCount; i++) {
-    fragment.appendChild(getPhotoTemplate(getPhotoObject(minLikes, maxLikes, commentsArray, commentsCount, photoCount)[i]));
+    fragment.appendChild(getPhotoTemplate(getPhotoObject(minLikes, maxLikes, commentsCount, photoCount)[i]));
   }
 
   pictureElement.appendChild(fragment);
 };
 
-renderPhotos(MIN_LIKES, MAX_LIKES, COMMENTS_ARRAY, COMMENTS_COUNT, PHOTO_COUNT);
+renderPhotos(MIN_LIKES, MAX_LIKES, COMMENTS_COUNT, PHOTO_COUNT);
