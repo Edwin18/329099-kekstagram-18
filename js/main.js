d626c12d022df7aae5dfc16b2bf0753045c4346d
Edwin18 <igreenbergi78@gmail.com>
2019-09-23 20:03:21 +0300

    Лайки

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@
 var COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 var DESCRIPTION = ['Тестим новую зеркалку!*)', 'Тестим новый телефон!^)', 'Тестим новый фотик!:)', 'Тестим новую камеру!-)'];
 var LIKES = {
-  min: 15,
-  max: 250
+  MIN: 15,
+  MAX: 250
 };
 var PHOTO_COUNT = 25;
 var AVATAR_COUNT = 6;
@@ -50,7 +50,7 @@ var getRandomComments = function () {
 var getPhotoObject = function (count) {
   return {
     url: 'photos/' + (count + 1) + '.jpg',
-    likes: getRandomNumber(LIKES.min, LIKES.max),
+    likes: getRandomNumber(LIKES.MIN, LIKES.MAX),
     comments: getRandomComments(),
     description: DESCRIPTION[getRandomNumber(0, DESCRIPTION.length - 1)]
   };
